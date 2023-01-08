32ea79ff2c77dd87ddb814de42506446d0952e10
Jake Lee <leejakejh@gmail.com>
2023-01-07 23:56:14 -0800

    solution added

---
+++ String-1/firstHalf.js
@@ -0,0 +1,13 @@
+// String-1 -- firstHalf
+// Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+// Examples
+
+// firstHalf('WooHoo') → WooHoo
+// firstHalf('HelloThere') → HelloThere
+// firstHalf('abcdefg') → abcdefg
+
+
+function firstHalf(str){
+  return str.substring(0);
+}
