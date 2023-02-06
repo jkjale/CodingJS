0d44d5a5d1dc350f29beca64b5b5d411ecd0bc22
Jake Lee <leejakejh@gmail.com>
2023-02-05 22:20:22 -0800

    solution added

---
+++ Logic-1/greenTicket.js
@@ -0,0 +1,26 @@
+// Logic-1 -- greenTicket
+// You have a green lottery ticket, with ints a, b, and c on it. If the numbers are all different from each other, the result is 0. If all of the numbers are the same, the result is 20. If two of the numbers are the same, the result is 10.
+
+// Examples
+
+// greenTicket(1, 2, 3) → 0
+// greenTicket(2, 2, 2) → 20
+// greenTicket(1, 1, 2) → 10
+
+
+function greenTicket(a, b, c){
+  if (a === b) {
+    if (b === c) {
+      return 20;
+    }
+    return 10;
+  } else if (a === c) {
+    return 10;
+  } else {
+    if (b !== c) {
+      return 0;
+    } else {
+      return 10;
+    }
+  }
+}
