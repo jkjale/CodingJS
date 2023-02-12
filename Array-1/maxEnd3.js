a1c0afd717b4ecdbbd255e05a44b5d10efee3aaf
Jake Lee <leejakejh@gmail.com>
2023-02-11 20:47:37 -0800

    solution added

---
+++ Array-1/maxEnd3.js
@@ -0,0 +1,19 @@
+// Array-1 -- maxEnd3
+// Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+
+// Examples
+
+// maxEnd3([1, 2, 3]) → 3,3,3
+// maxEnd3([11, 5, 9]) → 11,11,11
+// maxEnd3([2, 11, 3]) → 3,3,3
+
+
+function maxEnd3(nums){
+  let largerInt = (nums[0] > nums[nums.length-1]) ? nums[0] : nums[nums.length-1];
+  
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = largerInt;
+  }
+  
+  return nums;
+}
---
+++ Array-1/reverse3.js
@@ -0,0 +1,18 @@
+// Array-1 -- reverse3
+// Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+
+// Examples
+
+// reverse3([1, 2, 3]) → 3,2,1
+// reverse3([5, 11, 9]) → 9,11,5
+// reverse3([7, 0, 0]) → 0,0,7
+
+
+function reverse3(nums){
+  let firstIndexNum = nums[0];
+  
+  nums[0] = nums[nums.length-1];
+  nums[nums.length-1] = firstIndexNum;
+  
+  return nums;
+}
