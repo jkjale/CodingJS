c67aa99bcee1d47d6fa3563e9a4b842de25e2d61
Jake Lee <leejakejh@gmail.com>
2023-03-07 18:07:19 -0800

    solution added

---
+++ Recursion-1/factorial.js
@@ -0,0 +1,17 @@
+// Recursion-1 -- factorial
+// Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+
+// Examples
+
+// factorial(1) → 1
+// factorial(2) → 2
+// factorial(3) → 6
+
+
+function factorial(n){
+  if (n === 1) {
+    return 1;
+  }
+ 
+  return n * factorial(n-1);
+}
